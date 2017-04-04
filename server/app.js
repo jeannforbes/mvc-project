4f53b424df45212a32a363d095e1c37aef478ced
jeannforbes <jeannette.forbes@gmail.com>
2017-04-04 14:08:56 -0400

    Redis saves sessions

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const url = require('url');
+const RedisStore = require('connect-redis')(session);
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -18,6 +20,18 @@ mongoose.connect(dbURL, (err) => {
 		throw err;
 	}
 });
+
+let redisURL = {
+	hostname: 'localhost',
+	port: 6379,
+};
+
+let redisPASS;
+
+if(process.env.REDISCLOUD_URL){
+	redisURL = url.parse(process.env.REDISCLOUD_URL);
+	redisPASS = redisURL.auth.split(':')[1];
+}
 
 const router = require('./router.js');
 
@@ -34,6 +48,11 @@ app.use(session({
 	secret: 'Domo Arigato',
 	resave: true,
 	saveUninitialized: true,
+	store: new RedisStore({
+		host: redisURL.hostname,
+		port: redisURL.port,
+		pass: redisPASS,
+	}),
 }));
 
 app.engine('handlebars', expressHandlebars({defaultLayout:'main'}));
