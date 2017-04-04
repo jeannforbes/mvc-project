24b9d8a1992ecbe785a8b4e6708d6a53ba7f0b3c
jeannforbes <jeannette.forbes@gmail.com>
2017-04-04 15:13:25 -0400

    Revert "Added CSRF tokens"
    This reverts commit 5e075ec586f14d88a910a903c191c2f212a5eb7e.

--- server/app.js
+++ server/app.js
@@ -9,7 +9,6 @@ const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
 const url = require('url');
 const RedisStore = require('connect-redis')(session);
-const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -39,7 +38,6 @@ const router = require('./router.js');
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
 	extended: true,
@@ -55,9 +53,6 @@ app.use(session({
 		port: redisURL.port,
 		pass: redisPASS,
 	}),
-	cookie: {
-		httpOnly: true,
-	},
 }));
 
 app.engine('handlebars', expressHandlebars({defaultLayout:'main'}));
@@ -69,14 +64,6 @@ app.engine('handlebars', expressHandlebars({
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
-
-app.use(csrf());
-app.use((err, req, res, next) => {
-	if(err.code !== 'EBADCSRFTOKEN') return next(err);
-
-	console.log('Missing CSRF token');
-	return false;
-})
 
 router(app);
 
